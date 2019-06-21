b5ba69bd66e05fef9e37a1f0ef576ea9bf30181d
Sean Hill <sean.daryl.hill@gmail.com>
2019-06-21 07:18:36 -0700

    fix(zip) replaced zip with tar

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
+process.env.AWS_REGION = process.env.AWS_REGION || 'us-east-1'
+
 const fs = require('fs')
-const unzipper = require('unzipper')
+const tar = require('tar')
 const childProcess = require('child_process')
+const awsSdk = require('aws-sdk')
 
 exports.handler = async (event, context) => {
+  console.time('total')
   const downloadedFilePath = await this.download()
   const unzippedPath = await this.extract(downloadedFilePath)
   const stdout = await this.execute(unzippedPath)
+  console.timeEnd('total')
   return this.response(stdout)
 }
 
 exports.download = async () => {
-  return './zipper.zip'
+  console.time('download')
+  const s3 = new awsSdk.S3()
+  const response = await s3
+    .getObject({
+      Bucket: 'zipperless',
+      Key: 'zipper.tar.gz'
+    })
+    .promise()
+
+  const zippedFilePath = '/tmp/zipper.zip'
+  fs.writeFileSync(zippedFilePath, response.Body)
+
+  console.timeEnd('download')
+  return zippedFilePath
 }
 
 exports.extract = async zippedFilePath => {
+  console.time('extract')
+
   const unzippedPath = '/tmp/zippered'
-  await new Promise(resolve => {
-    fs.createReadStream(zippedFilePath)
-      .pipe(unzipper.Extract({ path: unzippedPath }))
-      .on('close', resolve)
+  await tar.x({
+    file: zippedFilePath,
+    cwd: unzippedPath
   })
+
+  console.timeEnd('extract')
   return unzippedPath
 }
 
 exports.execute = async unzippedPath => {
-  return new Promise((resolve, reject) => {
+  console.time('execute')
+
+  const stdout = await new Promise((resolve, reject) => {
     const process = childProcess.fork(`${unzippedPath}/index.js`, [], {
       silent: true
     })
@@ -33,6 +56,9 @@ exports.execute = async unzippedPath => {
     process.stdout.on('data', msg => (stdout += msg))
     process.on('exit', () => resolve(stdout))
   })
+
+  console.timeEnd('execute')
+  return stdout
 }
 
 exports.response = stdout => ({
